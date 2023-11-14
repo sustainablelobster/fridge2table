4082bf611826ffe8b6e32ddf59a2c4685407da6f
blacknnwa <118859560+blacknnwa@users.noreply.github.com>
2023-11-14 05:26:15 -0500

    Create static.js

---
+++ fridge2table/static.js
@@ -0,0 +1,10 @@
+function displayInput() {
+      // Get the input values from the form
+      var ingredients = document.getElementById("ingredients").value;
+        
+      // Create a message to display
+      var message = ingredients
+        
+      // Update the HTML content to display the input data
+     document.getElementById("displayArea").innerHTML = message;
+     }
